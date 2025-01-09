ecbe6cbce25f940b999b3fd017c12f8d316c1463
Gerardo1909 <gerardotoboso1909@gmail.com>
2025-01-09 17:40:29 -0300

    Cambiando el requirements.txt

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='repofutbol',
-    version='0.1',
+    name='analisis_de_futbol',
+    version='1.0.1',
+
+    url='https://github.com/Gerardo1909/analisis_de_futbol',
+    author='Gerardo1909',
+    author_email='getobosobarrios@estudiantes.unsam.edu.ar',    
+
     packages=find_packages(),
-    install_requires=[]
+    install_requires=['pandas','matplotlib','seaborn','numpy','scikit-learn','scipy']
 )
